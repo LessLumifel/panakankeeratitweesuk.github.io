75221ed026c1007a39fc979da0e25f39e9165387
Panakan Keeratitweesuk <110042563+LessLumifel@users.noreply.github.com>
2023-05-21 16:04:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,375 @@
+var product = [{
+    id: 1,
+    img: 'https://mana-thailand.net/wp-content/uploads/2022/12/gluta-collagen-2free4.jpg',
+    name: 'Mana Collagen + Mana Gluta 2 แถม 4 ชุดอาหารผิวขาวใส คอลลาเจนญาญ่า',
+    price: 1850,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Gluta Collagen นวัตกรรมใหม่ ที่เป็นมากกว่าคำว่า "คอลลาเจนผิวขาว" เพื่อผลลัพธ์ผิวสุขภาพดี ผิวนุ่มขึ้น ผิวกระจ่างใส รูขุมขนกระชับริ้วรอยลดลง ผิมแข็งแรงมีออร่า (แบรนด์ Mana Collagen) อาหารเสริมความงาม',
+    type: '1'
+}, {
+    id: 2,
+    img: 'https://mana-thailand.net/wp-content/uploads/2022/12/mana-gluta-3free7.jpg',
+    name: 'Mana Gluta Collagen 3 แถม 7 ฟื้นผิวขาวใส',
+    price: 2650,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Gluta Collagen นวัตกรรมใหม่ ที่เป็นมากกว่าคำว่า "คอลลาเจนผิวขาว" เพื่อผลลัพธ์ผิวสุขภาพดี ผิวนุ่มขึ้น ผิวกระจ่างใส รูขุมขนกระชับริ้วรอยลดลง ผิมแข็งแรงมีออร่า (แบรนด์ Mana Collagen) อาหารเสริมความงาม',
+    type: '1'
+}, {
+    id: 3,
+    img: 'https://mana-thailand.net/wp-content/uploads/2022/12/mana-gluta-2free4-1.jpg',
+    name: 'Mana Gluta Collagen 2 แถม 4 ฟื้นผิวขาวใส',
+    price: 1850,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Gluta Collagen นวัตกรรมใหม่ ที่เป็นมากกว่าคำว่า "คอลลาเจนผิวขาว" เพื่อผลลัพธ์ผิวสุขภาพดี ผิวนุ่มขึ้น ผิวกระจ่างใส รูขุมขนกระชับริ้วรอยลดลง ผิมแข็งแรงมีออร่า (แบรนด์ Mana Collagen) อาหารเสริมความงาม',
+    type: '1'
+},{
+    id: 4,
+    img: 'https://mana-thailand.net/wp-content/uploads/2022/12/mana-premium2-free-vitc2.jpg',
+    name: 'Mana Collagen 2 แถม Vit-C 2 ขุดผิวขาวใสเพิ่มภูมิคุ้มกัน',
+    price: 1380,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Nutri Vit C วิตามินซี ช่วยให้ผิวใสสุขภาพดี และช่วยเสริมภูมิคุ้มกัน วิตซีเต็มโดส เหมือนได้ทานส้มสด ๆ จากสวน อาหารเสริมความงาม, อาหารเสริมสุขภาพ',
+    type: '2'
+},{
+    id: 5,
+    img: 'https://mana-thailand.net/wp-content/uploads/2022/12/mana-premium-3free7.jpg',
+    name: 'Mana Premium Collagen 3 แถม 7 คอลลาเจนผิวใส คอลลาเจนญาญ่า ผิวนุ่ม เนียนใส มีออร่า ลดสิว ฝ้ากระ',
+    price: 2650,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Nutri Vit C วิตามินซี ช่วยให้ผิวใสสุขภาพดี และช่วยเสริมภูมิคุ้มกัน วิตซีเต็มโดส เหมือนได้ทานส้มสด ๆ จากสวน อาหารเสริมความงาม, อาหารเสริมสุขภาพ',
+    type: '2'
+},{
+    id: 6,
+    img: 'https://mana-thailand.net/wp-content/uploads/2023/03/mana-prolean-matcha-3.jpg',
+    name: 'Mana Prolean เวย์โปรตีน รสมัทฉะลาเต้',
+    price: 1650,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Prolean โปรตีนลีนไขมัน เร่งการเผาผลาญ โปรตีนจากพืช 100% อาหารเสริมรูปร่าง พร้อมมากระชากหุ่นเก่าคุณแล้ว',
+    type: '3'
+},{
+    id: 7,
+    img: 'https://mana-thailand.net/wp-content/uploads/2023/03/mana-prolean-banana-3.jpg',
+    name: 'Mana Prolean เวย์โปรตีน รสนมกล้วยเกาหลี',
+    price: 1650,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Prolean โปรตีนลีนไขมัน เร่งการเผาผลาญ โปรตีนจากพืช 100% อาหารเสริมรูปร่าง พร้อมมากระชากหุ่นเก่าคุณแล้ว',
+    type: '3'
+},{
+    id: 8,
+    img: 'https://mana-thailand.net/wp-content/uploads/2023/03/mana-prolean-strawberry-3.jpg',
+    name: 'Mana Prolean เวย์โปรตีน รสสตอเบอรี่มิลเชค',
+    price: 1650,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Prolean โปรตีนลีนไขมัน เร่งการเผาผลาญ โปรตีนจากพืช 100% อาหารเสริมรูปร่าง พร้อมมากระชากหุ่นเก่าคุณแล้ว',
+    type: '3'
+},{
+    id: 9,
+    img: 'https://mana-thailand.net/wp-content/uploads/2023/03/mana-prolean-almond-3.jpg',
+    name: 'Mana Prolean เวย์โปรตีน รสวนิลาอัลมอนต์',
+    price: 1650,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Prolean โปรตีนลีนไขมัน เร่งการเผาผลาญ โปรตีนจากพืช 100% อาหารเสริมรูปร่าง พร้อมมากระชากหุ่นเก่าคุณแล้ว',
+    type: '3'
+},{
+    id: 10,
+    img: 'https://mana-thailand.net/wp-content/uploads/2023/03/mana-prolean-dark-chocolate-3.jpg',
+    name: 'Mana Prolean เวย์โปรตีน รสดาร์คโกโก้',
+    price: 1650,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Prolean โปรตีนลีนไขมัน เร่งการเผาผลาญ โปรตีนจากพืช 100% อาหารเสริมรูปร่าง พร้อมมากระชากหุ่นเก่าคุณแล้ว',
+    type: '3'
+},{
+    id: 11,
+    img: 'https://mana-thailand.net/wp-content/uploads/2023/03/mana-prolean-matcha.jpg',
+    name: 'Mana Prolean เวย์โปรตีน รสมัทฉะลาเต้',
+    price: 590,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Prolean โปรตีนลีนไขมัน เร่งการเผาผลาญ โปรตีนจากพืช 100% อาหารเสริมรูปร่าง พร้อมมากระชากหุ่นเก่าคุณแล้ว',
+    type: '3'
+},{
+    id: 12,
+    img: 'https://mana-thailand.net/wp-content/uploads/2023/03/mana-prolean-banana.jpg',
+    name: 'Mana Prolean เวย์โปรตีน รสนมกล้วยเกาหลี',
+    price: 590,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Prolean โปรตีนลีนไขมัน เร่งการเผาผลาญ โปรตีนจากพืช 100% อาหารเสริมรูปร่าง พร้อมมากระชากหุ่นเก่าคุณแล้ว',
+    type: '3'
+},{
+    id: 13,
+    img: 'https://mana-thailand.net/wp-content/uploads/2023/03/mana-prolean-almond.jpg',
+    name: 'Mana Prolean เวย์โปรตีน รสวนิลาอัลมอนต์',
+    price: 590,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Prolean โปรตีนลีนไขมัน เร่งการเผาผลาญ โปรตีนจากพืช 100% อาหารเสริมรูปร่าง พร้อมมากระชากหุ่นเก่าคุณแล้ว',
+    type: '3'
+},{
+    id: 14,
+    img: 'https://mana-thailand.net/wp-content/uploads/2023/03/mana-prolean-strawberry.jpg',
+    name: 'Mana Prolean เวย์โปรตีน รสสตอเบอรี่มิลเชค',
+    price: 590,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Prolean โปรตีนลีนไขมัน เร่งการเผาผลาญ โปรตีนจากพืช 100% อาหารเสริมรูปร่าง พร้อมมากระชากหุ่นเก่าคุณแล้ว',
+    type: '3'
+},{
+    id: 15,
+    img: 'https://mana-thailand.net/wp-content/uploads/2023/03/mana-prolean-dark-chocolate.jpg',
+    name: 'Mana Prolean เวย์โปรตีน รสดาร์คโกโก้',
+    price: 590,
+    description: 'ผลิตภัณฑ์เสริมอาหาร Mana Prolean โปรตีนลีนไขมัน เร่งการเผาผลาญ โปรตีนจากพืช 100% อาหารเสริมรูปร่าง พร้อมมากระชากหุ่นเก่าคุณแล้ว',
+    type: '3'
+}];
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        html += `<div onclick="openProductDetail(${i}) " class="product-items ${product[i].type}">
+            <img class="product-img" src="${product[i].img}" alt="">
+            <div style="font-size: 1.2vw;">${product[i].name}</div>
+            <div style="font-size: 1vw;">${numberWithCommas(product[i].price)} THB</div>
+        </div>`;
+    }
+    $("#productlist").html(html);
+}
+)
+
+//ใส่ , บนตัวเลข
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function search(elem) {
+    //console.log(elem.id)
+    var value = $('#' + elem.id).val()
+    console.log(value)
+
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        if (product[i].name.includes(value)) {
+            html += `<div onclick="openProductDetail(${i}) class="product-items ${product[i].type}">
+            <img class="product-img" src="${product[i].img}" alt="">
+            <div style="font-size: 1.2vw;">${product[i].name}</div>
+            <div style="font-size: 1vw;">${numberWithCommas(product[i].price)} THB</div>
+        </div>`;
+        }
+    }
+    if (html == '') {
+        $("#productlist").html(' <p> Not found Product</p>');
+    } else {
+        $("#productlist").html(html);
+    }
+}
+
+function menu(choice) {
+    console.log(choice);
+    if (choice == 'product') {
+        $("#homepage").hide();
+        $("#productpage").show();
+    } else if (choice == 'home'){
+        $("#homepage").show();
+        $("#productpage").hide();
+    }
+}
+
+
+
+function searchproduct(param) {
+    console.log(param)
+    $(".product-items").css('display', 'none')
+    if (param == 'all') {
+        $(".product-items").css('display', 'block')
+    }
+    else {
+        $("." + param).css('display', 'block')
+    }
+}
+
+var productindex = 0;
+function openProductDetail(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modalDesc").css('display', 'flex')
+    $("#mdd-img").attr('src', product[index].img);
+    $("#mdd-name").text(product[index].name);
+    $("#mdd-price").text(numberWithCommas(product[index].price) + " THB");
+    $("#mdd-desc").text(product[index].description);
+}
+
+function closeModal() {
+    $(".modal").css('display', 'none')
+}
+
+var cart = [];
+function addtocart() {
+    var pass = true;
+
+    for (let i = 0; i < cart.length; i++) {
+            if (productindex == cart[i].index) {
+                console.log('found same product')
+                cart[i].count++;
+                pass = false;
+            }    
+    }
+
+    if (pass) {
+        var obj = {
+            index: productindex,
+            id : product[productindex].id,
+            name:product[productindex].name,
+            price:product[productindex].price,
+            img:product[productindex].img,
+            count: 1
+        };
+        cart.push(obj)
+    }
+    console.log(cart)
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Add ' + product[productindex].name + ' to cart !'
+    })
+    $('#cartcount').css('display','flex').text(cart.length)
+}
+
+function openCart(){
+    $('#modalCart').css('display','flex')
+    rendercart();
+}
+
+// สร้างฟังก์ชันเพื่อรับค่าผลรวมและส่งไปยังหน้าอื่น
+function getTotalPrice() {
+    var totalPrice = 0;
+    for (let i = 0; i < cart.length; i++) {
+        totalPrice += cart[i].price * cart[i].count;
+    }
+    return totalPrice;
+}
+
+function updateModalCart() {
+    var mycartContent = $("#mycart").html();
+    var totalPriceContent = $("#totalPrice").html();
+    $("#checkoutMycart").html(mycartContent);
+    $("#checkoutTotalPrice").html(totalPriceContent);
+}
+
+function buy() {
+    var paymentMethod = $("input[name='paymentMethod']:checked").val();
+    var fullName = $("#fullName").val();
+    var address = $("#address").val();
+    var city = $("#city").val();
+    var postalCode = $("#postalCode").val();
+
+    // Perform further actions based on selected payment method and delivery address
+    // You can use the obtained values to process the purchase or perform any necessary validations
+    Swal.fire({
+        icon: 'success',
+        title: 'Check Out Success!'
+    });
+    
+    // Reset the form and clear the values
+    resetForm();
+}
+
+function resetForm() {
+    // Reset the form HTML
+    document.getElementById("myForm").reset();
+
+    // Clear the values in other variables or states as needed
+    paymentMethod = null;
+    fullName = '';
+    address = '';
+    city = '';
+    postalCode = '';
+
+    // Clear the selected payment method
+    var paymentOptions = document.getElementsByName("paymentMethod");
+    for (var i = 0; i < paymentOptions.length; i++) {
+        paymentOptions[i].checked = false;
+    }
+
+    // Reset additional elements or states to their default values
+    // Example:
+    $("#totalPrice").text("ราคารวม: 0 THB");
+    $("#mycart").empty();
+
+    // Reset cart count
+    cartCount = 0;
+    updateCartCount();
+
+    // Refresh the page
+    location.reload();
+}
+
+
+
+// ในส่วนของ rendercart()
+function rendercart() {
+    if (cart.length > 0) {
+        var html = '';
+        for (let i = 0; i < cart.length; i++) {
+            html += `
+            <div class="cartlist-items">
+                <div class="cartlist-left">
+                    <img src="${cart[i].img}" alt="">
+                    <div class="cartlist-detail">
+                        <p style="font-size: 1.5vw;">${cart[i].name}</p>
+                        <p style="font-size: 1.2vw;">${numberWithCommas(cart[i].price * cart[i].count)} THB</p>
+                    </div>
+                </div>
+                <div class="cartlist-right">
+                    <p onclick="deinitems('-', ${i})" class="btnc">-</p>
+                    <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
+                    <p onclick="deinitems('+', ${i})" class="btnc">+</p>
+                </div>
+            </div>`;
+        }
+        $("#mycart").html(html);
+
+        // เรียกใช้ฟังก์ชัน getTotalPrice() เพื่อรับค่าผลรวม
+        var totalPrice = getTotalPrice();
+
+        $("#totalPrice").text(`ราคารวม: ${numberWithCommas(totalPrice)} THB`);
+    } else {
+        $("#mycart").html(`<p> Not Found Product list</p>`);
+        $("#totalPrice").text(`ราคารวม: 0 THB`);
+    }
+}
+
+
+function deinitems(action, index) {
+    if(action == '-') {
+        if(cart[index].count > 0) {
+            cart[index].count--;
+            $("#countitems"+index).text(cart[index].count)
+
+            if(cart[index].count <= 0) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Are you sure to delete?',
+                    showConfirmButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Delete',
+                    cancelButtonText: 'Cancel'
+                }).then((res) => {
+                  if(res.isConfirmed) {
+                     cart.splice(index, 1) 
+                     console.log(cart)
+                     rendercart();
+                     $("#cartcount").css('display','flex').text(cart.length)
+                     
+                     if(cart.length <= 0) {
+                        $("#cartcount").css('display','none')
+                     }
+                  }  
+                  else {
+                    cart[index].count++;
+                    $("#countitems"+index).text(cart[index].count)
+                    rendercart();
+                  }
+                })
+            }
+            rendercart();
+        }
+        
+    }
+    else if(action == '+') {
+        cart[index].count++;
+        $("#countitems"+index).text(cart[index].count)
+        rendercart();
+    }
+}
+
+function opencheckout(){
+    $('#checkoutCart').css('display','flex')
+    rendercart();
+}
+
